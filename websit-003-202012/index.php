ba8c93ef09c58150b6301f52d84825ea477b4ee5
x010403 <x010403@C1703476.PROD.SITAD.DK>
2020-11-04 16:36:04 +0100

    opstart-20201104-sdfe-commit-002 opretter projekt ved at henter filer fra site. Efetsom git for netbeans peget på website-003 inkluderes filer automatisk

---
+++ websit-003-202012/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>This is my title nb009-testing</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'This is my body nb009-3-4  (netbeans-test)';
+        echo '---';
+        phpinfo();
+        
+        $test=" youtbe Rajesh RK sætter netbean-xdebug op i php.ini"
+        ?>
+    </body>
+</html>
